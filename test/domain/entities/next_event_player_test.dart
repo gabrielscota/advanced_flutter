9ca4ef5890d7184331ab7aec41cb3c172c1563f4
Gabriel Scotá <gabrielscota2015@gmail.com>
2025-08-21 21:03:51 -0300

    🔨: refactor NextEventPlayer to use a final initials field and update initialization logic

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -3,24 +3,40 @@ import 'package:flutter_test/flutter_test.dart';
 class NextEventPlayer {
   final String id;
   final String name;
-  late final String initials;
+  final String initials;
   final String? photo;
   final String? position;
   final bool isConfirmed;
   final DateTime? confirmationDate;
 
-  NextEventPlayer({
+  NextEventPlayer._({
     required this.id,
     required this.name,
+    required this.initials,
     this.photo,
     this.position,
     required this.isConfirmed,
     this.confirmationDate,
-  }) {
-    initials = _getInitials();
-  }
+  });
 
-  String _getInitials() {
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    String? photo,
+    String? position,
+    required bool isConfirmed,
+    DateTime? confirmationDate,
+  }) => NextEventPlayer._(
+    id: id,
+    name: name,
+    initials: _getInitials(name),
+    photo: photo,
+    position: position,
+    isConfirmed: isConfirmed,
+    confirmationDate: confirmationDate,
+  );
+
+  static String _getInitials(String name) {
     final List<String> nameParts = name.split(' ');
 
     return '${nameParts.first[0]}${nameParts.last[0]}';
