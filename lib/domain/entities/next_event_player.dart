02259c8fab99668dc50c67772b61f594f9208403
Gabriel Scotá <gabrielscota2015@gmail.com>
2025-08-26 12:05:37 -0300

    🔨: move next player entity to correct folder

---
+++ lib/domain/entities/next_event_player.dart
@@ -0,0 +1,44 @@
+class NextEventPlayer {
+  final String id;
+  final String name;
+  final String initials;
+  final String? photo;
+  final String? position;
+  final bool isConfirmed;
+  final DateTime? confirmationDate;
+
+  NextEventPlayer._({
+    required this.id,
+    required this.name,
+    required this.initials,
+    this.photo,
+    this.position,
+    required this.isConfirmed,
+    this.confirmationDate,
+  });
+
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    String? photo,
+    String? position,
+    required bool isConfirmed,
+    DateTime? confirmationDate,
+  }) => NextEventPlayer._(
+    id: id,
+    name: name,
+    initials: _getInitials(name),
+    photo: photo,
+    position: position,
+    isConfirmed: isConfirmed,
+    confirmationDate: confirmationDate,
+  );
+
+  static String _getInitials(String name) {
+    final List<String> nameParts = name.toUpperCase().trim().split(' ');
+    final String firstChar = nameParts.first.split('').firstOrNull ?? '-';
+    final String lastChar = nameParts.last.split('').elementAtOrNull(nameParts.length == 1 ? 1 : 0) ?? '';
+
+    return '$firstChar$lastChar';
+  }
+}
