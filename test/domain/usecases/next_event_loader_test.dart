8f22f1870c546bb6f768946207144d6fa48b689b
Gabriel Scotá <gabrielscota2015@gmail.com>
2025-08-26 23:45:09 -0300

    🔧: add NextEventLoaderUseCase and LoadNextEventRepository with initial test case

---
+++ test/domain/usecases/next_event_loader_test.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+
+class NextEventLoaderUseCase {
+  final LoadNextEventRepository repository;
+
+  NextEventLoaderUseCase({required this.repository});
+
+  Future<void> call({required String groupId}) async {
+    await repository.loadNextEvent(groupId: groupId);
+  }
+}
+
+class LoadNextEventRepository {
+  String? groupId;
+  int callCount = 0;
+
+  Future<void> loadNextEvent({required String groupId}) async {
+    this.groupId = groupId;
+    callCount++;
+  }
+}
+
+void main() {
+  test('should load event data from a repository', () async {
+    // Arrange
+    final String groupId = Random().nextInt(50000).toString();
+    final LoadNextEventRepository repository = LoadNextEventRepository();
+    final NextEventLoaderUseCase sut = NextEventLoaderUseCase(repository: repository);
+
+    // Act
+    await sut(groupId: groupId);
+
+    // Assert
+    expect(repository.groupId, groupId);
+    expect(repository.callCount, 1);
+  });
+}
